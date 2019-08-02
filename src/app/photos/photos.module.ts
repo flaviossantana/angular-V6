ba58025ea25142611e174ea80fbe4f692e6a4323
Flavio Santana <9090flavio@gmail.com>
2019-08-01 22:28:43 -0300

    First commit

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {PhotoComponent} from './photo/photo.component';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [
+    PhotoComponent
+  ],
+  exports: [
+    PhotoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PhotosModule {
+}
