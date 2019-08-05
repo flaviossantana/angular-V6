888381c4fddf406af385bbda2b878ffdb7c77461
Flavio Santana <9090flavio@gmail.com>
2019-08-05 20:47:51 -0300

    Utilizando a primeira diretiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alura PIC!';
-
+  fotos = [
+    {
+      'descricao': 'Ford Bravo',
+      'local': 'https://visualhunt.com/photos/1/blue-vintage-car-in-front-of-house.jpg'
+    },
+    {
+      'opacidade': '0.8',
+      'descricao': 'Carro abandonado',
+      'local': 'https://visualhunt.com/photos/1/oldtimer-car-vintage-old-headlight-engine-hood.jpg'
+    },
+    {
+      'descricao': 'Barco em alto mar',
+      'local': 'https://visualhunt.com/photos/1/boat-water-sea-ship.jpg'
+    }
+  ];
 }
