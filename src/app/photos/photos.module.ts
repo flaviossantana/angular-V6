01de03c117113303f75bda4f8ab9d55b7843ff61
Flavio Santana <9090flavio@gmail.com>
2019-08-06 20:47:40 -0300

    Isolando a acesso em serviços

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {PhotoComponent} from './photo/photo.component';
 import {CommonModule} from '@angular/common';
-import {PhotoService} from './photo.service';
 
 @NgModule({
   declarations: [
