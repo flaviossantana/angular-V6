cfb770b5b8588a1fe6d118282635bf883358fb36
Flavio Santana <9090flavio@gmail.com>
2019-08-07 21:01:14 -0300

    BrowserModule e CommonModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {PhotosModule} from './photos/photos.module';
-import {HttpClientModule} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
