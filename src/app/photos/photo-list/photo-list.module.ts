355ef8467ec9a2791fbdeb04081251f5c1591856
Flavio Santana <9090flavio@gmail.com>
2019-08-20 21:09:21 -0300

    07/02 Submódulos

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -3,7 +3,6 @@ import {PhotoListComponent} from './photo-list.component';
 import {PhotosComponent} from './photos/photos-component';
 import {BotaoCarregarComponent} from './botao-carregar/botao-carregar.component';
 import {OrdenarPorPipe} from '../../pipe/ordenar-por.pipe';
-import {PhotoComponent} from '../photo/photo.component';
 import {CommonModule} from '@angular/common';
 import {FiltroPorDescricaoPipe} from '../filtro-por-descricao.pipe';
 import {PhotoModule} from '../photo/photo.module';
