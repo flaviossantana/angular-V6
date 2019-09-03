a1dbe8447b931dc7f9e5597ac85ab87329984968
Flavio Santana <9090flavio@gmail.com>
2019-09-02 22:48:05 -0300

    01/04 Validação de formulários

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,14 @@
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
 import {LoginComponent} from './login/login.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   declarations: [
     LoginComponent
   ]
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,9 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'ap-login',
+  styleUrls: ['login.component.scss'],
   templateUrl: 'login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      usuario: ['', Validators.required],
+      senha: ['', [
+        Validators.required,
+        Validators.minLength(4)
+        ]
+      ]
+    });
+  }
 
 }
