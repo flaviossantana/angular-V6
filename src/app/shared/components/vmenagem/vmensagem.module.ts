6885baf841ed8a93a9f8855f713c1672526a598d
Flávio Santana <9090flavio@gmail.com>
2019-09-27 21:55:14 -0300

    Correção do layout

---
+++ src/app/shared/components/vmenagem/vmensagem.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {VmessageComponent} from './vmessage.component';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [
+    VmessageComponent
+  ],
+  exports: [
+    VmessageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VmensagemModule {
+
+}
