50bcbe4cc36430b00842ba9be19b179e93fd3910
Flávio Santana <9090flavio@gmail.com>
2019-09-27 21:55:07 -0300

    Correção do layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 
 import {LoginComponent} from './login/login.component';
 import {CommonModule} from '@angular/common';
+import {VmensagemModule} from '../shared/components/vmenagem/vmensagem.module';
 
 @NgModule({
   imports: [
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    VmensagemModule
   ],
   declarations: [
     LoginComponent
