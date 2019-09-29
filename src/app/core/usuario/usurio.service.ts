70a25e0da153b2c4ff8903cfa171311ee324cffb
Flávio Santana <9090flavio@gmail.com>
2019-09-28 22:14:44 -0300

    [ANGULAR PARTE 2][AUTENTICAÇÃO FORMS E LAZY LOADING][SEPARAÇÃO DE RESPONSABILIDADES]

---
+++ src/app/core/usuario/usurio.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {TokenService} from '../token/token.service';
+import {Subject} from 'rxjs';
+import * as JWT from 'jwt-decode';
+
+import {Usuario} from './usuario';
+
+@Injectable({providedIn: 'root'})
+export class UsurioService {
+
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.getToken()) {
+      this.tokenService.getToken();
+    }
+  }
+
+  private usuarioSubject = new Subject<Usuario>();
+
+  setToken(chave: string) {
+    this.tokenService.setToken(chave);
+    this.decodificaENotifica();
+  }
+
+  getusuario() {
+    return this.usuarioSubject.asObservable();
+  }
+
+  private decodificaENotifica() {
+    const chave = this.tokenService.getToken();
+    const usuario = JWT(chave) as Usuario;
+    this.usuarioSubject.next(usuario);
+  }
+
+}
