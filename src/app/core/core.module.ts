e00ec8cf285e95f54d7d19dd430cb3716391a2cc
Flávio Santana <9090flavio@gmail.com>
2019-10-02 20:06:02 -0300

    [ANGULAR PARTE 2][AUTENTICAÇÃO FORMS E LAZY LOADING][A DIRETIVA ROUTERLINK]

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CabecalhoComponent} from './cabecalho/cabecalho.component';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     CabecalhoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     CabecalhoComponent
