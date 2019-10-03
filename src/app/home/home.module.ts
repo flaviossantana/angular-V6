0233127684786a97b1ac9391c269734fd0c6d234
Flávio Santana <9090flavio@gmail.com>
2019-10-02 21:42:01 -0300

    [ANGULAR PARTE 2][AUTENTICAÇÃO FORMS E LAZY LOADING][O COMPONENTE DE REGISTRO]

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,13 +5,15 @@ import {LoginComponent} from './login/login.component';
 import {CommonModule} from '@angular/common';
 import {VmensagemModule} from '../shared/components/vmensagem/vmensagem.module';
 import {InscreverComponent} from './inscrever/inscrever.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     ReactiveFormsModule,
     CommonModule,
     VmensagemModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   declarations: [
     LoginComponent,
