e1d7ec77fc172cd556175ebd8996f6025bb2e066
Flávio Santana <9090flavio@gmail.com>
2019-10-21 23:08:52 -0300

    [ANGULAR PARTE 3][UPLOAD, BUILD E NOVOS COMPONENTES][ADEQUANDO A EXIBIÇÃO DAS FOTOS]

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,12 +1,30 @@
 import {Component, Input} from '@angular/core';
 
+const NUVEM = 'http://localhost:3000/imgs/';
+
 @Component({
   selector: 'ap-photo',
   templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
 
-  @Input() local = '';
+  private _local = '';
+
   @Input() descricao = '';
   @Input() dataPostagem = '';
+
+  @Input() set local(local: string){
+    if(!local.startsWith('data')){
+      this._local = NUVEM + local;
+    } else {
+      this._local = local;
+    }
+
+
+  }
+
+  get local(){
+    return this._local;
+}
+
 }
