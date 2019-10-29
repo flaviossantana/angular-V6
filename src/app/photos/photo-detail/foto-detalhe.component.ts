a97a8a3bc6dcdc60e4857b1795cf9491cc5373b1
Flávio Santana <9090flavio@gmail.com>
2019-10-28 21:32:04 -0300

    [ANGULAR PARTE 3][DETALHES DA FOTO][EXTRAINDO PARÂMETROS DE ROTAS]

--- src/app/photos/photo-detail/foto-detalhe.component.ts
+++ src/app/photos/photo-detail/foto-detalhe.component.ts
@@ -1,8 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, ActivatedRouteSnapshot} from "@angular/router";
 
 @Component({
   templateUrl: 'foto-detalhe.component.html'
 })
-export class FotoDetalheComponent {
+export class FotoDetalheComponent implements OnInit{
+
+  constructor(
+    private route: ActivatedRoute){
+  }
+
+
+  ngOnInit(): void {
+    let fotoid = this.route.snapshot.params.fotoId;
+    console.log(fotoid);
+  }
 
 }
