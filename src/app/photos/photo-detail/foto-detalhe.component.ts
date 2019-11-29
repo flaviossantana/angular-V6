7742f617674b0c2089c16c0b08e01db3d5e7c082
Flávio Santana <9090flavio@gmail.com>
2019-11-28 22:34:41 -0300

    [ANGULAR PARTE 3][UPLOAD, BUILD E NOVOS COMPONENTES][05 REMOÇÃO DE FOTOS E NOVOS COMPONENTES][03 REMOÇÃO DE FOTOS]

--- src/app/photos/photo-detail/foto-detalhe.component.ts
+++ src/app/photos/photo-detail/foto-detalhe.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute} from "@angular/router";
 import {PhotoService} from "../photo.service";
 import {Photo} from "../photo";
 import {Comentario} from "../Comentario";
+import {UsuarioService} from "../../core/usuario/usuario.service";
+import {Observable} from "rxjs";
+import {Usuario} from "../../core/usuario/usuario";
 
 @Component({
   templateUrl: 'foto-detalhe.component.html',
@@ -12,16 +15,25 @@ export class FotoDetalheComponent implements OnInit {
 
   constructor(
     private fotoService: PhotoService,
+    private usuarioService: UsuarioService,
     private route: ActivatedRoute) {
   }
 
   foto: Photo;
+  usuarioLogado: Usuario;
   comentarios: Comentario[];
 
   ngOnInit(): void {
     let fotoid = this.route.snapshot.params.fotoId;
     this.buscarFoto(fotoid);
     this.buscarComentario(fotoid);
+    this.busacarUsuarioLogado();
+  }
+
+  private busacarUsuarioLogado() {
+    this.usuarioService.getusuario().subscribe(usuario => {
+      this.usuarioLogado = usuario;
+    });
   }
 
   private buscarComentario(fotoid) {
@@ -41,4 +53,9 @@ export class FotoDetalheComponent implements OnInit {
         this.foto = foto
       });
   }
+
+  isUsuarioLogado(){
+
+  }
+
 }
